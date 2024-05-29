57fe8519c5982168d3113ea94e6dde3e004983ed
peten1105 <peten@kakao.com>
2024-05-29 21:38:05 +0900

    Init
    JPA 연결된 프로젝트

---
+++ lemon-ticket/src/main/java/com/flab/lemonticket/LemonTicketApplication.java
@@ -0,0 +1,13 @@
+package com.flab.lemonticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LemonTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LemonTicketApplication.class, args);
+	}
+
+}
---
+++ lemon-ticket/src/test/java/com/flab/lemonticket/LemonTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.lemonticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LemonTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
